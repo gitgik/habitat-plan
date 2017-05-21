311e6483014f128af7234b919d2c37864a7474ad
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-05-21 15:18:13 +0300

    [chore] define build and installation callbacks to be called at buildtime

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -12,3 +12,36 @@ pkg_exposes=(port)
 pkg_bin_dirs=(bin)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
+
+
+do_download() {
+  return 0
+}
+
+do_verify() {
+  return 0
+}
+
+do_unpack() {
+  return 0
+}
+
+do_build() {
+  # copy the contents of the source directory to the habitat cache path
+  cp -vr $PLAN_CONTEXT/../source/*  $HAB_CACHE_SRC_PATH/$pkg_dirname
+
+  # install the npm dev dependencies listed in package.json
+  npm install
+}
+
+do_install() {
+  # copy the node scripts into the root directory of our package using the pkg_prefix variable
+  cp package.json ${pkg_prefix}
+  cp server.js ${pkg_prefix}
+
+  # cope the node modules into the package using the pkg_prefix variable
+  mkdir -p ${pkg_prefix}/node_modules/
+  cp -vr node_modules/* ${pkg_prefix}/node_modules
+}
+
+do_
