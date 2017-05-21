a60048b28a0b44ec702e58cb519a6c5496a3d84d
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-05-21 15:01:08 +0300

    [chore] update plan to include the pkg configurations

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,1 +1,11 @@
-
+pkg_origin=nerd
+pkg_name=myapp
+pkg_version=0.1.0
+pkg_maintainer="The Habitat Maintainers <jeegiks@gmail.com>"
+pkg_upstream_url="https://github.com/gitgik/habitat-plan"
+pkg_source=nosuchfile.tar.gz
+pkg_deps=(core/node)
+pkg_exports=(
+  [port]=listening_port
+)
+pkg_exposes=(port)
